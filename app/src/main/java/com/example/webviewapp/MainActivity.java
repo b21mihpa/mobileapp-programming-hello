d1b081644d2f9c94c2bce9237ca8faf0039e0408
b21mihpa <b21mihpa@student.his.se>
2022-03-31 09:47:29 +0200

    updated: TextView in content_main to display today's date

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.webviewapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
 
+        TextView textView = findViewById(R.id.currentDate);
+        String date = new SimpleDateFormat("EEEE, dd/MM/yyyy", Locale.getDefault()).format(new Date());
+        textView.setText(textView.getText() + ": " + date);
+    }
 }
